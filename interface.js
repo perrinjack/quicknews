428c089e3f4117a084f8f9647225a9995cb70b69
J Perrin <perrinjack96@gmail.com>
2020-06-27 14:20:50 +0100

    renderHeadlines now does not take an argument

--- interface.js
+++ interface.js
@@ -1,10 +1,10 @@
 window.addEventListener(`DOMContentLoaded`, () => {
   var storyhandler = new StoryHandler();
-  renderHeadlines(storyhandler);
+  renderHeadlines();
 
-  function renderHeadlines(input) {
-    input.stories.then(function (allStories) {
-      document.getElementById('app').innerHTML = input.wrapHeadlines(
+  function renderHeadlines() {
+    storyhandler.stories.then(function (allStories) {
+      document.getElementById('app').innerHTML = storyhandler.wrapHeadlines(
         allStories
       );
     });
