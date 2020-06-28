5fa5a2b578f637b83807ae679379650e060ad6ad
J Perrin <perrinjack96@gmail.com>
2020-06-28 15:49:39 +0100

    altered rendering of thumbnails, so image link results in hashchange & hashchange link can be unique for each story

--- src/interface.js
+++ src/interface.js
@@ -4,13 +4,10 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
   window.addEventListener('hashchange', function () {
     // var id = window.location.hash.slice('#')[6];
-    
-    document.getElementById(`page`).innerHTML = "<div class='row fh5co-post-entry single-entry'><article class='col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0'><figure><img src='images/hello.jpg' class='img-responsive'></figure><div class='col-md-12'><p>Far far away, behind the word mountains, ff sentences fly into your mouth.</p></div></article></div>"
-  });
 
-  
-						
-  
+    document.getElementById(`page`).innerHTML =
+      "<div class='row fh5co-post-entry single-entry'><article class='col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0'><figure><img src='images/hello.jpg' class='img-responsive'></figure><div class='col-md-12'><p>Far far away, behind the word mountains, ff sentences fly into your mouth.</p></div></article></div>";
+  });
 
   function renderNews() {
     storyhandler.stories.then(function (allStories) {
@@ -40,7 +37,7 @@ window.addEventListener(`DOMContentLoaded`, () => {
       for (i = 0; i < allStories.length; i++) {
         document.getElementById(
           `image${i + 1}`
-        ).innerHTML = `<img src='${allStories[i].thumbnail}' class="img-responsive"></img>`;
+        ).innerHTML = `<a href="#hh"><img src='${allStories[i].thumbnail}' class="img-responsive"></img></a>`;
       }
     }
   }
