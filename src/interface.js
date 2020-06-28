333839dbe352496ab070b77e688bb726a9f6fca1
J Perrin <perrinjack96@gmail.com>
2020-06-28 15:40:28 +0100

    on hash change new page is shown

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,16 @@ window.addEventListener(`DOMContentLoaded`, () => {
   var storyhandler = new StoryHandler();
   renderNews();
 
+  window.addEventListener('hashchange', function () {
+    // var id = window.location.hash.slice('#')[6];
+    
+    document.getElementById(`page`).innerHTML = "<div class='row fh5co-post-entry single-entry'><article class='col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0'><figure><img src='images/hello.jpg' class='img-responsive'></figure><div class='col-md-12'><p>Far far away, behind the word mountains, ff sentences fly into your mouth.</p></div></article></div>"
+  });
+
+  
+						
+  
+
   function renderNews() {
     storyhandler.stories.then(function (allStories) {
       renderHeadlines(allStories);
