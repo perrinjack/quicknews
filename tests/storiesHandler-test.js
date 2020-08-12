1f73d090f241345af78304bc2058dbc22f0f37c6
J Perrin <perrinjack96@gmail.com>
2020-08-12 10:12:08 +0100

    Removal of own testing framework

--- tests/storiesHandler-test.js
+++
@@ -1,9 +0,0 @@
-// Testing here has been spiked. Promise is received.
-
-// function storesStoryObjects() {
-//   storyhandler = new StoryHandler();
-//   console.log(storyhandler.getStories());
-//   assert.isTrue(storyhandler.getStories().length === 10);
-// }
-
-// storesStoryObjects();
